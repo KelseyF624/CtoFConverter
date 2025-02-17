eab43a87e3783ce145b86480639ca0ccfa98f887
KelseyF624 <kelseyf.624@gmail.com>
2025-02-17 16:03:14 -0500

    Complete Commit

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 public class CtoFConverter {
     public static void main(String[] args) {
-        do {
-        System.out.println("What is the temperature in Celsius?");
+        double temperature = 0;
+        double fahrenheit;
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter 'C' to convert to Fahrenheit: ");
-        Scanner input2 = new Scanner(System.in);
-        double temperature = input.nextDouble();
-        double fahrenheit = temperature * 1.8 + 32;
-        if (input2.next().equals("C")) {
-            System.out.println("The temperature in Fahrenheit is " + fahrenheit);
-        } else {
-            System.out.println("Invalid entry. Please try again.");
-        }
-        System.out.print("Enter Yes to try again and No to exit: ");
-        Scanner input3 = new Scanner(System.in);
-        input3.nextLine();
-        } while (args.next().equals("Yes"));
+        boolean valid;
+        do {
+            System.out.println("What is the temperature in Celsius?");
+            if (input.hasNextDouble()) {
+                temperature = input.nextDouble();
+                valid = true;
+            } else {
+                valid = false;
+                input.next();
+                System.out.println("Please enter a number.");}
+        }while (!valid);
+        fahrenheit = temperature * 1.8 + 32;
+        System.out.println("The temperature in Fahrenheit is " + fahrenheit);
     }
 }
